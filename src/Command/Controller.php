e3b394c11928657f80e8f7f5e68923aa5d65f0cb
RezaParsian <rezaparsian76@gmail.com>
2022-01-22 17:04:00 +0330

    try add module binding to controller

--- src/Command/Controller.php
+++ src/Command/Controller.php
@@ -48,7 +48,7 @@ class Controller extends GeneratorCommand
 
         $this->line("<fg=yellow>It may take a few second...</>");
 
-        if (fileExists(base_path("modules/{$module}/Controllers/{$name}.php")) and !$this->option("force")) {
+        if (file_exists(base_path("modules/{$module}/Controllers/{$name}.php")) and !$this->option("force")) {
             $this->line("<fg=red>Controller already exists.</>");
             return;
         }
@@ -80,6 +80,10 @@ class Controller extends GeneratorCommand
 
         if ($type = $this->option('type')) {
             $stub = "/stubs/controller.{$type}.stub";
+        } elseif ($this->option('parent')) {
+            $stub = '/stubs/controller.nested.stub';
+        } elseif ($this->option('model')) {
+            $stub = '/stubs/controller.model.stub';
         } elseif ($this->option('invokable')) {
             $stub = '/stubs/controller.invokable.stub';
         } elseif ($this->option('resource')) {
@@ -104,8 +108,8 @@ class Controller extends GeneratorCommand
             ['type', null, InputOption::VALUE_REQUIRED, 'Manually specify the controller stub file to use.'],
             ['force', null, InputOption::VALUE_NONE, 'Create the class even if the controller already exists'],
             ['invokable', 'i', InputOption::VALUE_NONE, 'Generate a single method, invokable controller class.'],
-//            ['model', 'm', InputOption::VALUE_OPTIONAL, 'Generate a resource controller for the given model.'],
-//            ['parent', 'p', InputOption::VALUE_OPTIONAL, 'Generate a nested resource controller class.'],
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'Generate a resource controller for the given model.'],
+            ['parent', 'p', InputOption::VALUE_OPTIONAL, 'Generate a nested resource controller class.'],
             ['resource', 'r', InputOption::VALUE_NONE, 'Generate a resource controller class.'],
             ['requests', 'R', InputOption::VALUE_NONE, 'Generate FormRequest classes for store and update.'],
         ];
@@ -118,5 +122,119 @@ class Controller extends GeneratorCommand
     protected function getNamespace($name)
     {
         return "Modules\\" . $this->argument('module') . "\\Controllers";
+    }
+
+
+    /**
+     * Build the model replacement values.
+     *
+     * @param array $replace
+     * @return array
+     */
+    protected function buildModelReplacements(array $replace)
+    {
+        $modelClass = $this->parseModel($this->option('model'));
+
+        if (!class_exists($modelClass)) {
+            if ($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
+                $this->call('make:model', ['name' => $modelClass]);
+            }
+        }
+
+        $replace = $this->buildFormRequestReplacements($replace, $modelClass);
+
+        return array_merge($replace, [
+            'DummyFullModelClass' => $modelClass,
+            '{{ namespacedModel }}' => $modelClass,
+            '{{namespacedModel}}' => $modelClass,
+            'DummyModelClass' => class_basename($modelClass),
+            '{{ model }}' => class_basename($modelClass),
+            '{{model}}' => class_basename($modelClass),
+            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
+            '{{ modelVariable }}' => lcfirst(class_basename($modelClass)),
+            '{{modelVariable}}' => lcfirst(class_basename($modelClass)),
+        ]);
+    }
+
+    protected function buildClass($name)
+    {
+        $controllerNamespace = $this->getNamespace($name);
+
+        $replace = [];
+
+        if ($this->option('parent')) {
+            $replace = $this->buildParentReplacements();
+        }
+
+
+        if ($this->option('model')) {
+            $replace = $this->buildModelReplacements($replace);
+        }
+
+        $replace["use {$controllerNamespace}\Controller;\n"] = '';
+
+
+        return str_replace(
+            array_keys($replace), array_values($replace), parent::buildClass($name)
+        );
+    }
+
+
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string  $model
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        return $this->qualifyModel($model);
+    }
+
+    /**
+     * Build the model replacement values.
+     *
+     * @param  array  $replace
+     * @param  string  $modelClass
+     * @return array
+     */
+    protected function buildFormRequestReplacements(array $replace, $modelClass)
+    {
+        [$namespace, $storeRequestClass, $updateRequestClass] = [
+            'Illuminate\\Http', 'Request', 'Request',
+        ];
+
+        if ($this->option('requests')) {
+            $namespace = 'App\\Http\\Requests';
+
+            [$storeRequestClass, $updateRequestClass] = $this->generateFormRequests(
+                $modelClass, $storeRequestClass, $updateRequestClass
+            );
+        }
+
+        $namespacedRequests = $namespace.'\\'.$storeRequestClass.';';
+
+        if ($storeRequestClass !== $updateRequestClass) {
+            $namespacedRequests .= PHP_EOL.'use '.$namespace.'\\'.$updateRequestClass.';';
+        }
+
+        return array_merge($replace, [
+            '{{ storeRequest }}' => $storeRequestClass,
+            '{{storeRequest}}' => $storeRequestClass,
+            '{{ updateRequest }}' => $updateRequestClass,
+            '{{updateRequest}}' => $updateRequestClass,
+            '{{ namespacedStoreRequest }}' => $namespace.'\\'.$storeRequestClass,
+            '{{namespacedStoreRequest}}' => $namespace.'\\'.$storeRequestClass,
+            '{{ namespacedUpdateRequest }}' => $namespace.'\\'.$updateRequestClass,
+            '{{namespacedUpdateRequest}}' => $namespace.'\\'.$updateRequestClass,
+            '{{ namespacedRequests }}' => $namespacedRequests,
+            '{{namespacedRequests}}' => $namespacedRequests,
+        ]);
     }
 }
--- src/Command/Model.php
+++ src/Command/Model.php
@@ -10,6 +10,7 @@ use Illuminate\Routing\Console\ControllerMakeCommand;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 use Rp76\Module\Helper\Command as RpCommand;
+use Symfony\Component\Console\Input\InputOption;
 use function PHPUnit\Framework\fileExists;
 
 class Model extends GeneratorCommand
@@ -44,16 +45,19 @@ class Model extends GeneratorCommand
 
         $this->line("<fg=yellow>It may take a few second...</>");
 
-        Artisan::call("make:model " . "../../modules/{$module}/Models/{$name}" . ($this->option("m") != 1 ? " -m" : ""));
+//        Artisan::call("make:model " . "../../modules/{$module}/Models/{$name}" . ($this->option("m") != 1 ? " -m" : ""));
+//
+//        file_put_contents(base_path("modules/{$module}/Models/{$name}.php"), str_replace("App\Models\..\..\m", "M", file_get_contents(base_path("modules/{$module}/Models/{$name}.php"))));
+//
+//        if ($this->option("m") != 1) {
+//            $files = glob("database/migrations/*" . strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', "create_{$name}")) . "*");
+//            $files = end($files);
+//            $fileName = explode("/", $files);
+//            rename($files, base_path("modules/{$module}/Migrations/" . end($fileName)));
+//        }
 
-        file_put_contents(base_path("modules/{$module}/Models/{$name}.php"), str_replace("App\Models\..\..\m", "M", file_get_contents(base_path("modules/{$module}/Models/{$name}.php"))));
-
-        if ($this->option("m") != 1) {
-            $files = glob("database/migrations/*" . strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', "create_{$name}")) . "*");
-            $files = end($files);
-            $fileName = explode("/", $files);
-            rename($files, base_path("modules/{$module}/Migrations/" . end($fileName)));
-        }
+        $this->files->makeDirectory("modules/{$module}/Models", 0777, true, true);
+        $this->files->put(base_path("modules/{$module}/Models/{$name}.php"), $this->sortImports($this->buildClass($name)));
 
         $this->line("<fg=green>model created successfully.</>");
     }
@@ -67,15 +71,42 @@ class Model extends GeneratorCommand
 
     protected function resolveStubPath($stub)
     {
-        $stubPath = str_replace("/ControllerMakeCommand.php", "", (new \ReflectionClass(ModelMakeCommand::class))->getFileName()) . $stub;
+        $stubPath = str_replace("/ModelMakeCommand.php", "", (new \ReflectionClass(ModelMakeCommand::class))->getFileName()) . $stub;
 
         return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
             ? $customPath
             : $stubPath;
     }
 
+    private function addOptions()
+    {
+        $options = [
+            ['all', 'a', InputOption::VALUE_NONE, 'Generate a migration, seeder, factory, policy, and resource controller for the model'],
+            ['controller', 'c', InputOption::VALUE_NONE, 'Create a new controller for the model'],
+            ['factory', 'f', InputOption::VALUE_NONE, 'Create a new factory for the model'],
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the model already exists'],
+            ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model'],
+            ['policy', null, InputOption::VALUE_NONE, 'Create a new policy for the model'],
+            ['seed', 's', InputOption::VALUE_NONE, 'Create a new seeder for the model'],
+            ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model'],
+            ['resource', 'r', InputOption::VALUE_NONE, 'Indicates if the generated controller should be a resource controller'],
+            ['api', null, InputOption::VALUE_NONE, 'Indicates if the generated controller should be an API controller'],
+            ['requests', 'R', InputOption::VALUE_NONE, 'Create new form request classes and use them in the resource controller'],
+        ];
+
+        foreach ($options as $option) {
+            $this->addOption(...$option);
+        }
+    }
+
     public function __construct(Filesystem $files)
     {
         parent::__construct($files);
+        $this->addOptions();
+    }
+
+    protected function getNamespace($name)
+    {
+        return "Modules\\" . $this->argument('module') . "\\Models";
     }
 }
